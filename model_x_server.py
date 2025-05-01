4c456126161e13ed19a7b4d4cb826c675cad17f9
Nicholas Mesa-Cucalon <nmesacuc@andrew.cmu.edu>
2025-04-30 21:40:02 -0400

    code for feint

--- model_x_server.py
+++ model_x_server.py
@@ -20,8 +20,8 @@ from collections import defaultdict, deque
 Model Setup
 """
 print("Loading Model...")
-model_path = "./best_large.engine" 
-# model_path = "./best_large.pt"
+# model_path = "./best_large.engine" 
+model_path = "./best_large.pt"
 model = YOLO(model_path)
 print("Done! \n")
 
@@ -77,6 +77,7 @@ class Box:
         self.cls_id = cls_id
         self.conf_score = conf_score
         self.box = box_coords
+        self.has_fb = False
         self.dealer_box = False
 
     # String Method
@@ -98,12 +99,10 @@ def regex(s):
         return ''  # for empty strings
 
 while True:
-    """
-    Detection Code
-    """
+    # Data Structures
     all_boxes = []
     fb_boxes  = []
-    seen_cls = set()
+    seen_cls = {}
     coords_to_cls_conf = {}
     cls_to_coords = {}
 
@@ -111,12 +110,15 @@ while True:
     ret, frame = cap.read()
     if not ret:
         print("Error: Failed to grab frame.")
-        exit(0)
+        break
 
     # Perform detections
     result = model(frame)[0]
 
-    # Establish all_boxes
+    
+    """
+    Processing
+    """
     for i in range(len(result.boxes)):
         # Get detection data
         box = result.boxes[i]
@@ -143,27 +145,19 @@ while True:
         # Case 3: Repeat Detections
         if curr_cls in seen_cls:
             # Get past data
+            box = seen_cls[curr_cls]
+            box.has_fb = True
             past_x1, past_y1, past_x2, past_y2 = cls_to_coords[curr_cls]
             # NOTE: fb = full body
             fb_x1, fb_y1 = min(curr_x1, past_x1), min(curr_y1, past_y1)
             fb_x2, fb_y2 = max(curr_x2, past_x2), max(curr_y2, past_y2)
-            # Determine if vertical or horizontal and proceed accordingly
-            vertical = abs(curr_x2 - curr_x1) < abs(curr_y2 - curr_y1)
-            x_dist, y_dist = abs(fb_x2 - fb_x1), abs(fb_y2 - fb_y1)
-            if debug:
-                print(x_dist)
-                print(y_dist)
-            if (vertical and x_dist <= fb_x) or (not vertical and y_dist <= fb_y):
-                # The repeat detection is the same card, so create a fb_box
-                fb_coords = (fb_x1, fb_y1, fb_x2, fb_y2)
-                fb_box    = Box(fb_coords, curr_cls, curr_conf)
-                fb_boxes.append(fb_box)
-                continue
+            fb_coords    = (fb_x1, fb_y1, fb_x2, fb_y2)
+            fb_box       = Box(fb_coords, curr_cls, curr_conf)
+            fb_boxes.append(fb_box)
+            all_boxes.append(fb_box)
+            continue
 
         # Base Case: New Card Detected (Update local structures)
-
-        # Add the cls to the seen_cls
-        seen_cls.add(curr_cls)
 
         # Add coords to the map
         coords_to_cls_conf[coords] = (curr_cls, curr_conf)
@@ -172,93 +166,125 @@ while True:
         cls_to_coords[curr_cls] = coords
 
         # Add box to all_boxes
-        all_boxes.append(Box(coords, curr_cls, curr_conf))
-        
+        tmp_box = Box(coords, curr_cls, curr_conf)
+        all_boxes.append(tmp_box)
+
+        # Add the cls to the seen_cl
+        seen_cls[curr_cls] = tmp_box
 
     """
-    Data Processing
+    Box Display
     """
-    # Find the dealers hand using fb_boxes
-    y_tol = 60
-    y_vals = []
-    for box in fb_boxes:
-        _, y1, _, y2 = box.box
-        mid_y = (y1 + y2) / 2
-        y_vals.append(mid_y)
-        # print(f"fb_box = {box}, mid_y = {mid_y}")
-    y_vals.sort()
-
-    # Find the middle element of the sorted y_vals
-    try:
-        avg_y = int(y_vals[len(y_vals) // 2])
-    except:
-        avg_y = 0
-
     for box in all_boxes:
-        _, y1, _, y2 = box.box
-        mid_y = (y1 + y2) / 2
-        if (mid_y <= avg_y - y_tol) or (avg_y + y_tol <= mid_y):
-            box.dealer_box = True
-            # print(f"dealers hand = {box}")
+        if not box.has_fb:
+            curr_cls = box.cls_id
+            curr_conf = box.conf_score
+            curr_x1, curr_y1, curr_x2, curr_y2 = box.box
 
-    # Clustering
-    tl_coords = []
-    dealer_cards = []
-    for box in all_boxes:
-        # Keep only the x1 coordinate
-        if not box.dealer_box:
-            x1, _, _, _ = box.box
-            tl_coords.append((x1,box))
-        else:
-            dealer_cards.append(box)
-
-    # Sort the tl coords to then cluster
-    tl_coords.sort(key = lambda x: x[0])
-
-    # Cluster assuming the boxes have a distance of x_tol between them
-    try:
-        clusters = [[tl_coords[0]]]
-        for i in range(1, len(tl_coords)):
-            prev = tl_coords[i - 1][0]
-            curr = tl_coords[i][0]
-            if curr - prev > x_tol:
-                # Start a new cluster
-                clusters.append([tl_coords[i]])
-            else:
-                # Add to current cluster
-                clusters[-1].append(tl_coords[i])
-    except:
-        clusters = [[]]
+            # Print data to CLI
+            print(f"Detected: {model.names[curr_cls]} | Confidence: {curr_conf:.2f} | BBox: ({curr_x1}, {curr_y1}), ({curr_x2}, {curr_y2})")
+            
+            # Draw Rectangles
+            color = colors[curr_cls % len(colors)] 
+            cv2.rectangle(frame, (curr_x1, curr_y1), (curr_x2, curr_y2), color, 2)
+            cv2.putText(frame, f"{model.names[curr_cls]} {curr_conf:.2f}", (curr_x1, curr_y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     """
-    Data Transfer
+    Frame
     """
-    dealer_hand = [regex(str(model.names[box.cls_id])) for box in dealer_cards]
-    player_hands = [{"id" : "dealer", "hand": [dealer_hand]}]
-    counter = 1
-    for cluster in clusters:
-        hand = []
-        for elt in cluster:
-            box = elt[1]
-            rank = regex(str(model.names[box.cls_id]))
-            hand.append(rank)
-        player_hands.append({"id" : str(counter), "hand": [hand]})
-        counter += 1
+    cv2.imshow("Camera Feed", frame)
 
-    data = {"player_hands" : player_hands}
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    # Print data to CLI
-    print(f"Player Hands after Processing = {player_hands}")
+
+
+
+
+    # """
+    # Data Processing
+    # """
+    # # Find the dealers hand using fb_boxes
+    # y_tol = 60
+    # y_vals = []
+    # for box in fb_boxes:
+    #     _, y1, _, y2 = box.box
+    #     mid_y = (y1 + y2) / 2
+    #     y_vals.append(mid_y)
+    #     # print(f"fb_box = {box}, mid_y = {mid_y}")
+    # y_vals.sort()
+
+    # # Find the middle element of the sorted y_vals
+    # try:
+    #     avg_y = int(y_vals[len(y_vals) // 2])
+    # except:
+    #     avg_y = 0
+
+    # for box in all_boxes:
+    #     _, y1, _, y2 = box.box
+    #     mid_y = (y1 + y2) / 2
+    #     if (mid_y <= avg_y - y_tol) or (avg_y + y_tol <= mid_y):
+    #         box.dealer_box = True
+    #         # print(f"dealers hand = {box}")
+
+    # # Clustering
+    # tl_coords = []
+    # dealer_cards = []
+    # for box in all_boxes:
+    #     # Keep only the x1 coordinate
+    #     if not box.dealer_box:
+    #         x1, _, _, _ = box.box
+    #         tl_coords.append((x1,box))
+    #     else:
+    #         dealer_cards.append(box)
+
+    # # Sort the tl coords to then cluster
+    # tl_coords.sort(key = lambda x: x[0])
+
+    # # Cluster assuming the boxes have a distance of x_tol between them
+    # try:
+    #     clusters = [[tl_coords[0]]]
+    #     for i in range(1, len(tl_coords)):
+    #         prev = tl_coords[i - 1][0]
+    #         curr = tl_coords[i][0]
+    #         if curr - prev > x_tol:
+    #             # Start a new cluster
+    #             clusters.append([tl_coords[i]])
+    #         else:
+    #             # Add to current cluster
+    #             clusters[-1].append(tl_coords[i])
+    # except:
+    #     clusters = [[]]
+
+    # """
+    # Data Transfer
+    # """
+    # dealer_hand = [regex(str(model.names[box.cls_id])) for box in dealer_cards]
+    # player_hands = [{"id" : "dealer", "hand": [dealer_hand]}]
+    # counter = 1
+    # for cluster in clusters:
+    #     hand = []
+    #     for elt in cluster:
+    #         box = elt[1]
+    #         rank = regex(str(model.names[box.cls_id]))
+    #         hand.append(rank)
+    #     player_hands.append({"id" : str(counter), "hand": [hand]})
+    #     counter += 1
+
+    # data = {"player_hands" : player_hands}
+
+    # # Print data to CLI
+    # print(f"Player Hands after Processing = {player_hands}")
     
-    """
-    Server Comms
-    """
-    # Emit the event your server listens for
-    print("Sending card_data event...")
-    sio.emit("card_data", data)
+    # """
+    # Server Comms
+    # """
+    # # Emit the event your server listens for
+    # print("Sending card_data event...")
+    # sio.emit("card_data", data)
 
-    # Wait a bit to allow processing
-    time.sleep(0.2)
+    # # Wait a bit to allow processing
+    # time.sleep(0.2)
 
 # Done!
 sio.disconnect()

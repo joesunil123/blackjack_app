6a3ff27b1c0a1d05dba490fa31a480481d25febb
Joe Sunil <joesunilinchodikaran@Joes-Air-2.icloud.com>
2025-04-30 21:07:29 -0400

    Last PUSH

--- test_push.py
+++ test_push.py
@@ -18,43 +18,49 @@ def disconnect():
 sio.connect("http://127.0.0.1:5050/")  # Adjust port if different
 # END TO COPY
 
-
-cards = deque(["8", "A", "4", "3", "6", "10", "4", "8", "5", "10"])
-dealer_hand = []
-players = [[] for _ in range(4)]
- 
-counter = 0
-mp = [0, 1, 2, 3]
-random.shuffle(mp)
 while True:
-    if cards:
-        if counter == 4:
-            dealer_hand.append(cards[0])
-        else:
-            players[counter].append(cards[0])
-        
-        counter = (counter + 1) % 5
-        cards.popleft()
+    cards = deque(["8", "A", "4", "3", "6", "10", "4", "8", "5", "10"])
+    dealer_hand = []
+    players = [[] for _ in range(4)]
     
-    # Simulate pushing card stream data to the backend
-    sample_data = {
-        "player_hands": [
-            {"id": "dealer", "hand": [dealer_hand.copy()]},
-            {"id": "1", "hand": [players[mp[0]].copy()]},
-            {"id": "2", "hand": [players[mp[1]].copy()]},
-            {"id": "3", "hand": [players[mp[2]].copy()]},
-            {"id": "4", "hand": [players[mp[3]].copy()]}
-        ],
-    }
+    counter = 0
+    mp = [0, 1, 2, 3]
+    random.shuffle(mp)
+    while cards:
+        debug = False
+        if cards:
+            if counter == 4:
+                debug = True
+                dealer_hand.append(cards[0])
+            else:
+                players[counter].append(cards[0])
+            
+            counter = (counter + 1) % 5
+            cards.popleft()
+        
+        # Simulate pushing card stream data to the backend
+        sample_data = {
+            "player_hands": [
+                {"id": "dealer", "hand": [dealer_hand.copy()]},
+                {"id": "1", "hand": [players[mp[0]].copy()]},
+                {"id": "2", "hand": [players[mp[1]].copy()]},
+                {"id": "3", "hand": [players[mp[2]].copy()]},
+                {"id": "4", "hand": [players[mp[3]].copy()]}
+            ],
+        }
 
-    time.sleep(0.2)
+        time.sleep(0.2)
 
-    # TO COPY
-    # Emit the event your server listens for
-    print("Sending card_data event...")
-    sio.emit("card_data", sample_data)
+        # TO COPY
+        # Emit the event your server listens for
+        t = 1
+        if debug:
+            t = 10
+        for _ in range(t):
+            print(f"Sending card_data event... {t}")
+            sio.emit("card_data", sample_data)
 
-    # Wait a bit to allow processing
-    time.sleep(0.2)
+        # Wait a bit to allow processing
+        time.sleep(2)
     
 sio.disconnect()
